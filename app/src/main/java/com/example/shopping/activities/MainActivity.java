fdd1ba1a3b31f7cee59540e35b8c750891b22b1e
mahmoud_osama <46555700+MahmoudOsamaAli@users.noreply.github.com>
2019-10-14 16:45:55 +0200

    14/10/2019 part2

--- app/src/main/java/com/example/shopping/activities/MainActivity.java
+++ app/src/main/java/com/example/shopping/activities/MainActivity.java
@@ -33,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if(savedInstanceState == null){
