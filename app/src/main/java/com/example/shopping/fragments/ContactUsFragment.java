94ad2f6c9353c055a34ff782f13e9c7da4e222ea
mahmoud_osama <46555700+MahmoudOsamaAli@users.noreply.github.com>
2019-10-16 17:04:08 +0200

    add new project fragment

--- app/src/main/java/com/example/shopping/fragments/ContactUsFragment.java
+++ app/src/main/java/com/example/shopping/fragments/ContactUsFragment.java
@@ -1,19 +1,22 @@
 package com.example.shopping.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import com.example.shopping.R;
-import com.example.shopping.model.FavoriteViewModel;
+import com.example.shopping.activities.MainActivity;
 
 public class ContactUsFragment extends Fragment {
 
-    public View onCreateView(@NonNull LayoutInflater inflater,ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.fragment_contact_us, container, false);
+
     }
 }
