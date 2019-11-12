a876de7c0e05d6ce8306e7d762c3512d1ff0c195
mahmoud_osama <46555700+MahmoudOsamaAli@users.noreply.github.com>
2019-11-12 17:37:52 +0200

    layout changes

--- app/src/main/java/com/example/expense/view/activities/Home/HomeActivity.java
+++ app/src/main/java/com/example/expense/view/activities/Home/HomeActivity.java
@@ -1,7 +1,11 @@
 package com.example.expense.view.activities.Home;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import com.example.expense.R;
 import com.example.expense.view.activities.signInUp.SignInActivity;
@@ -20,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -57,8 +62,9 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void init() {
-
         ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Expense");
         navDrawerConfig();
         bottomNavConfig();
     }
@@ -185,6 +191,26 @@ public class HomeActivity extends AppCompatActivity {
                 navView.setSelectedItemId(R.id.navigation_home);
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        try {
+            MenuInflater inflater = getMenuInflater();
+            inflater.inflate(R.menu.options_menu, menu);
+
+            // Associate searchable configuration with the SearchView
+            SearchManager searchManager =
+                    (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+            SearchView searchView =
+                    (SearchView) menu.findItem(R.id.search).getActionView();
+            searchView.setSearchableInfo(
+                    searchManager.getSearchableInfo(getComponentName()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
     }
 
 }
